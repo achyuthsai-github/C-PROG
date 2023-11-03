3bd49c57dc3907d3408237de60c4c7a484ef5503
achyuthsai-github <143405523+achyuthsai-github@users.noreply.github.com>
2023-11-03 23:44:17 +0530

    Add files via upload

---
+++ ALPHABET Pattern 3.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	int i,j;
+	for(i=0;i<5;i++)
+	{
+		for(j=4;j>=i;j--)
+		{
+			printf("%c",i+65);
+			
+			
+			
+			
+		} 
+		printf("\n");
+		
+		
+	}
+}
---
+++ alphabet pattern 2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	char i,j,count='A';
+	for(i='A';i<'F';i++)
+	{
+		for(j='A';j<=i;j++)
+		{
+			printf("%c",count);
+			
+			
+			
+		} 
+		printf("\n");
+		count++;
+	}
+}
